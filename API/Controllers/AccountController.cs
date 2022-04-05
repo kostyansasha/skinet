b270bf3124c3db8d46f0d0c46695b8aff4ed30b2
okostian <kostyansasha@ukr.net>
2022-04-05 15:12:38 +0200

    API - Validation

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -66,7 +66,7 @@ namespace API.Controllers
 
             var result = await _userManager.UpdateAsync(user);
 
-            if (result.Succeeded) 
+            if (result.Succeeded)
                 return Ok(_mapper.Map<Address, AddressDto>(user.Address));
 
             return BadRequest("Problem updating the user.");
@@ -94,6 +94,14 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse
+                {
+                    Errors = new[] { "Email Address is in use." }
+                });
+            }
+
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
---
+++ API/Dtos/BasketItemDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Dtos
+{
+    public class BasketItemDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string ProductName { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0.1, double.MaxValue, ErrorMessage = "Price must be greater than zero.")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(1, double.MaxValue, ErrorMessage = "Quantity must be at least 1.")]
+        public int Quantity { get; set; }
+
+        [Required]
+        public string PictureUrl { get; set; } = string.Empty;
+
+        [Required]
+        public string Brand { get; set; } = string.Empty;
+
+        [Required]
+        public string Type { get; set; } = string.Empty;
+    }
+}
