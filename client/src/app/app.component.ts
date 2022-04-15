b9577a350fb9f3aeb7bf9ab16389347019bd33df
okostian <kostyansasha@ukr.net>
2022-04-15 14:53:44 +0200

    Client - Identity

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 
 @Component({
@@ -9,9 +10,23 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit {
   title = 'Skinet';
 
-  constructor(private basketService: BasketService) { }
+  constructor(private basketService: BasketService, private accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+
+    this.accountService.loadCurrentUser(token).subscribe({
+      next: () => { console.log('user loaded'); },
+      error: (error: Error) => { console.log(error); }
+    });
+  }
+
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
 
     if (basketId) {
